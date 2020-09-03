312d03b3236fe64ed930e0a57feaa1c9793fe3f0
DenisViskov <DenisViskov16@gmail.com>
2020-09-03 17:06:09 +0300

    update

--- src/main/controller/HallServlet.java
+++ src/main/controller/HallServlet.java
@@ -1,11 +1,20 @@
 package controller;
 
+
+import org.apache.maven.lifecycle.internal.LifecycleStarter;
+import org.json.*;
+import persistence.PsqlStore;
+import service.Place;
+import service.Store;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author Денис Висков
@@ -24,5 +33,15 @@ public class HallServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doPost(req, resp);
+    }
+
+    private JSONArray aggregatePlace() {
+        JSONArray result = new JSONArray();
+        Store store = PsqlStore.instOf();
+        List<Place> hall = new ArrayList<>(store.getHall());
+        for (int i = 0; i< store.getRows().size();i++){
+
+        }
+        return result;
     }
 }
