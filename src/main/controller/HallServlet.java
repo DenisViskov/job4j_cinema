fb6459110aa61e47cd172b690717ab874ff1673b
DenisViskov <DenisViskov16@gmail.com>
2020-09-03 15:29:42 +0300

    update

---
+++ src/main/controller/HallServlet.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+/**
+ * @author Денис Висков
+ * @version 1.0
+ * @since 03.09.2020
+ */
+@WebServlet("/index")
+public class HallServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        req.getRequestDispatcher(req.getContextPath() + "/index.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
