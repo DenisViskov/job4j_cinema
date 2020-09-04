dea4f0b36ecd2fe79577c73d790d8baf2b35c7cb
DenisViskov <denisviskov16@gmail.com>
2020-09-04 15:40:48 +0300

    update

--- src/main/java/controller/HallServlet.java
+++ src/main/java/controller/HallServlet.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,9 +27,11 @@ public class HallServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        aggregatePlace();
-        req.setCharacterEncoding("UTF-8");
-        req.getRequestDispatcher(req.getContextPath() + "/index.jsp").forward(req, resp);
+        resp.setContentType("application/json");
+        PrintWriter writer = new PrintWriter(resp.getOutputStream());
+        JSONObject result = aggregatePlace();
+        writer.print(result);
+        writer.flush();
     }
 
     @Override
